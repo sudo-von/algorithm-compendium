b856dfc6d665b3fc369517d2ec0686c53524d01e
VoN <sudo.von.contact@gmail.com>
2025-09-11 21:55:21 -0600

    feat: ✨ add 13-roman-to-integer

---
+++ leetcode/algorithms/easy/13-roman-to-integer/solution.js
@@ -0,0 +1,28 @@
+const numerals = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        const current = numerals[s[i]];
+        const next = numerals[s[i+1]] ?? 0;
+        if (current < next) {
+            result += next - current;
+            i++;
+            continue;
+        }
+        result += current;
+    }
+    return result;
+};
